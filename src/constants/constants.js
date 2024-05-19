e537960eac43f3868af02a6b2da4806855e81cc6
mostaszewski <mostaszewski@medallia.com>
2024-05-19 20:28:48 +0200

    ProductForm - add list of options for keywords multi-select;

---
+++ src/constants/constants.js
@@ -0,0 +1,16 @@
+export const keywordsOptions = [
+  { label: "New", value: "New" },
+  { label: "Classic", value: "Classic" },
+  { label: "Modern", value: "Modern" },
+  { label: "Futuristic", value: "Futuristic" },
+  { label: "Hi-Fi", value: "Hi-Fi" },
+  { label: "Lo-Fi", value: "Lo-Fi" },
+  { label: "Kitchen appliance", value: "Kitchen appliance" },
+  { label: "Gardening", value: "Gardening" },
+  { label: "Health", value: "Health" },
+  { label: "Medical", value: "Medical" },
+  { label: "Luxurious", value: "Luxurious" },
+  { label: "Basic", value: "Basic" },
+  { label: "Professional", value: "Professional" },
+
+];
